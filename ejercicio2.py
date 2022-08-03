af8a5a5f8f311d7e805888601d6e22193fa731d6
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-03 11:23:14 -0300

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+print ('ingrese la nota del primer parcial');
+N1 = float(input('nota1'));
+print ('ingrese la nota del primer parcial');
+N2 = float(input('nota2'));
+print ('ingrese la nota del primer parcial');
+N3 = float(input('nota3'));
+
