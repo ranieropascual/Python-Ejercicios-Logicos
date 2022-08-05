66842a4986e41a148fcd5121b411e3e514c2502d
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-05 11:25:25 -0300

    ejercicio4

---
+++ ejercicio5.py
@@ -0,0 +1,9 @@
+print ('ingrese GB: ')
+GB=float(input())
+
+MG=GB*1024
+MD=MG/1.44
+
+print('\nSALIDA:')
+print('-----------------------------')
+print('numero de discos necesarios: math.ceil')
---
+++ ejercicio6.py
@@ -0,0 +1,7 @@
+BY=float(input('BY: '))
+
+D=((AX-BX)**2 + (AY-BY)**2 )**0.5
+
+print("\nSALIDA: ")
+print("-------------------------------------------------------")
+print("Resultado:", D)
