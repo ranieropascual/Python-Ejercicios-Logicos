abcabe0ab521a8050b3ba4b59cfecf3e3f0ee685
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-10 20:58:39 -0300

    corroborado

---
+++ ejercicio7.py
@@ -0,0 +1,15 @@
+print("-------------------------------------------------")
+print("ejercicio7: promedio de dos numeros")
+print("-------------------------------------------------")
+
+#entradas
+print("ingrese notas: ")
+N1= int(input("N1: "))
+N2= int(input("N2: "))
+
+#procesos
+S=N1+N2
+P=S/2
+
+#salida
+print("el promedio de las notas es: ",P)
