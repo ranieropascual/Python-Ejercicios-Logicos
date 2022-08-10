a59cc741bde9bafd85668b032efd564d9e01e71e
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-10 18:08:20 -0300

    corroborado

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,9 +1,21 @@
-print ('ingrese GB: ')
+import math
+
+
+print("------------------------------------------------------")
+print("ejercicio5: numero de micro discos 3.5 necesarios")
+print("------------------------------------------------------")
+
+#entradas
+print("ingrese GB: ")
 GB=float(input())
 
+#proceso
 MG=GB*1024
 MD=MG/1.44
 
-print('\nSALIDA:')
-print('-----------------------------')
-print('numero de discos necesarios: math.ceil')
+#salida
+print(MD)
+#en caso de decimal aproximar al siguiente entero
+#ya que la parte decimal debe ser almacenada en otro disco 3.5
+
+print("el numero de discos necesarios es: ",math.ceil(MD))
