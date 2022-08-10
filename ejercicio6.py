f4df5af28eeca95bf3f1d56f31a8cf7cb417658d
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-10 18:27:06 -0300

    corroborado

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,7 +1,19 @@
-BY=float(input('BY: '))
+print("------------------------------------------------------")
+print("ejercicio6: distancia entre dos puntos A y B, en 2D")
+print("------------------------------------------------------")
 
-D=((AX-BX)**2 + (AY-BY)**2 )**0.5
+#entradas
+print("ingrese coordenadas del punto A")
+AX=float(input("AX: "))
+AY=float(input("AY: "))
 
-print("\nSALIDA: ")
-print("-------------------------------------------------------")
-print("Resultado:", D)
+print("ingrese coordenadas del punto B")
+BX=float(input("BX: "))
+BY=float(input("BY: "))
+
+#proceso
+D=((AX-BX)**2+(BX-BY)**2)**0.5
+
+#salida
+print("el resultado es: ",D)
+
