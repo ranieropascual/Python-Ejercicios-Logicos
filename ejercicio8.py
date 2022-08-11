17c62b85f35bdfb912649412f1d4619032198097
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-10 21:08:34 -0300

    corroborado

---
+++ ejercicio8.py
@@ -0,0 +1,17 @@
+print("-----------------------------------------------------------")
+print("ejercicio8: calcular perimetro y superficie del rectangulo")
+print("-----------------------------------------------------------")
+
+#entradas
+print("ingrese base y alto")
+BASE=float(input("Base: "))
+ALTO=float(input("Alto: "))
+
+#procesos
+SUP=BASE*ALTO
+PER=2*(BASE+ALTO)
+
+#salida
+print ("-----------------------------------------------------------")
+print("superficie: ",SUP)
+print("perimetro: ",PER)
